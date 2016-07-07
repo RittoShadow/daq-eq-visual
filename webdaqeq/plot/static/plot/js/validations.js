d9de8352375384e4c3dff5eaeeb6d7a45005d6bf
Claudio Rojas R <claudio.rojas.rios@gmail.com>
2016-07-07 03:58:59 -0400

    blocked fields corresponding to certain conditions

--- webdaqeq/plot/static/plot/js/validations.js
+++ webdaqeq/plot/static/plot/js/validations.js
@@ -11,6 +11,61 @@ function enable_submit_span() {
 
 $(function () {
 
+  if (!$('#id_sendRecord')[0].checked) {
+    $('#id_username')[0].disabled = true;
+    $('#id_password')[0].disabled = true;
+    $('#id_structure')[0].disabled = true;
+    $('#id_email')[0].disabled = true;
+    $('#id_phoneNumber')[0].disabled = true;
+    $('#id_sendSMS')[0].disabled = true;
+    $('#id_compressRecord')[0].disabled = true;
+    $('#id_authenticationURL')[0].disabled = true;
+    $('#id_recordURL')[0].disabled = true;
+  }
+
+  if (!$('#id_sendStructHealth')[0].checked) {
+    $('#id_structHealthURL')[0].disabled = true;
+    $('#id_sendFrequency')[0].disabled = true;
+    $('#id_verificationFrequency')[0].disabled = true;
+  }
+
+  $('#id_sendRecord').change(function() {
+    if(this.checked) {
+        //Do stuff
+      $('#id_username')[0].disabled = false;
+      $('#id_password')[0].disabled = false;
+      $('#id_structure')[0].disabled = false;
+      $('#id_email')[0].disabled = false;
+      $('#id_phoneNumber')[0].disabled = false;
+      $('#id_sendSMS')[0].disabled = false;
+      $('#id_compressRecord')[0].disabled = false;
+      $('#id_authenticationURL')[0].disabled = false;
+      $('#id_recordURL')[0].disabled = false;
+    } else {
+      $('#id_username')[0].disabled = true;
+      $('#id_password')[0].disabled = true;
+      $('#id_structure')[0].disabled = true;
+      $('#id_email')[0].disabled = true;
+      $('#id_phoneNumber')[0].disabled = true;
+      $('#id_sendSMS')[0].disabled = true;
+      $('#id_compressRecord')[0].disabled = true;
+      $('#id_authenticationURL')[0].disabled = true;
+      $('#id_recordURL')[0].disabled = true;
+    }
+  });
+
+  $('#id_sendStructHealth').change(function () {
+    if(this.checked) {
+      $('#id_structHealthURL')[0].disabled = false;
+      $('#id_sendFrequency')[0].disabled = false;
+      $('#id_verificationFrequency')[0].disabled = false;
+    } else {
+      $('#id_structHealthURL')[0].disabled = true;
+      $('#id_sendFrequency')[0].disabled = true;
+      $('#id_verificationFrequency')[0].disabled = true;
+    }
+  });
+
   $('#config_form').submit(function(event) {
     // Obtener icon antiguo y reemplazar por reloj
     disable_submit_span();
